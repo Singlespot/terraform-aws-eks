818e5e5027be7ef72eef3b85a82acc640137beb9
Sébastien de Melo <sebastien@singlespot.com>
2020-03-12 18:14:19 +0100

    Added application deployment

---
+++ application/prometheus/prometheus.sh
@@ -0,0 +1,4 @@
+kubectl create namespace prometheus
+helm3 install prometheus stable/prometheus \
+     --namespace prometheus \
+     --set alertmanager.persistentVolume.storageClass="gp2",server.persistentVolume.storageClass="gp2"
