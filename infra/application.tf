02d31f8a9fe695b53e991927326e0b9ce485778d
Sébastien de Melo <sebastien.demelo@gmail.com>
2020-04-03 14:08:26 +0200

    Fixed typos

--- infra/application.tf
+++ infra/application.tf
@@ -1,19 +1,20 @@
 locals {
   kubeconfig                      = module.eks.kubeconfig_filename
   metrics_path                    = "scripts/metrics.sh"
-  metrics_destroy_path            = "scripts/metrics.sh"
+  metrics_destroy_path            = "scripts/metrics-destroy.sh"
   dashboard_path                  = "scripts/dashboard.sh"
-  dashboard_destroy_path          = "scripts/dashboard.sh"
+  dashboard_destroy_path          = "scripts/dashboard-destroy.sh"
   dashboard_url                   = "https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml"
   dashboard_k8s_path              = "k8s/dashboard/eks-admin-service-account.yaml"
   prometheus_path                 = "scripts/prometheus.sh"
-  prometheus_destroy_path         = "scripts/prometheus.sh"
+  prometheus_destroy_path         = "scripts/prometheus-destroy.sh"
   cluster_autoscaler_path         = "scripts/cluster_autoscaler.sh"
   cluster_autoscaler_destroy_path = "scripts/cluster_autoscaler-destroy.sh"
   cluster_autoscaler_version      = "v1.15.5"
 }
 
 resource "null_resource" "deploy_metrics" {
+//  depends_on = [module.eks]
   triggers = {
     script_md5 = filemd5(local.metrics_path)
   }
--- infra/scripts/prometheus.sh
+++ infra/scripts/prometheus.sh
@@ -2,6 +2,6 @@
 kubectl create namespace prometheus || true
 helm3 repo add stable https://kubernetes-charts.storage.googleapis.com
 helm3 repo update
-helm3 upgrade --install prometheus stable/prometheus \
+helm3 upgrade --install --values k8s/prometheus/values.yaml prometheus stable/prometheus \
       --namespace prometheus \
       --set alertmanager.persistentVolume.storageClass="gp2",server.persistentVolume.storageClass="gp2"
