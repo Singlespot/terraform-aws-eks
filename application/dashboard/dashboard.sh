7dca3a73f2df218021c4c7811757b8e93b6c2693
Sébastien de Melo <sebastien.demelo@protonmail.com>
2021-01-21 13:07:24 +0100

    Updated dashboard version

--- application/dashboard/dashboard.sh
+++ application/dashboard/dashboard.sh
@@ -1,2 +1,2 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
 kubectl apply -f eks-admin-service-account.yaml
